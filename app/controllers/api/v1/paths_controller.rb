15e3818eab09794f257e35096879bc372c808a5e
Kevin Carmona-Murphy <kcarmonamurphy@outlook.com>
2019-09-04 21:11:02 -0400

    path selection seems to be working for all 1+ levels

--- app/controllers/api/v1/paths_controller.rb
+++ app/controllers/api/v1/paths_controller.rb
@@ -8,10 +8,15 @@ module Api
         path_array = params[:path].split('/')
 
         first_record = Folder.find_by(name: path_array.shift)
-        last_record = get_path_record(first_record, path_array)
+        last_record = get_last_record(first_record, path_array)
 
-        response = { folder: last_record }
-        response.merge!({ images: last_record.images }) if last_record.present?
+        if last_record.is_a? Folder
+          response = { folder: last_record, images: last_record.images }
+        elsif last_record.is_a? Image
+          response = { image: last_record }
+        else
+          response = {}
+        end
 
         render json: response
       end
@@ -19,11 +24,18 @@ module Api
       private
 
       # method for recursively loading children records
-      def get_path_record(record, path_array)
+      def get_last_record(record, path_array)
         return record if path_array.empty?
 
-        child_record = record.children.find_by(name: path_array.shift)
-        get_path_record(child_record, path_array)
+        segment = path_array.shift
+
+        # get either the child folder or an image
+        folder = record.children.find_by(name: segment)
+        if folder.present?
+          get_last_record(folder, path_array)
+        else
+          return Image.find_by(name: segment, folder: record)
+        end
       end
 
       def set_image
