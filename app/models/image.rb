dfb3593f8d27c962baf9f45802af1d9e0daa886e
Kevin Carmona-Murphy <kcarmonamurphy@outlook.com>
2019-09-05 20:58:05 -0400

    fleshed out some of the specs, made them more robust

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,19 @@
 class Image < ApplicationRecord
   has_one_attached :file
 
-  belongs_to :folder
-
-  # has_ancestry
+  belongs_to :folder, optional: true
   
   def url
-    self.path_ids.map { |id| image.find(id).name }.join('/')
+    if self.folder.present?
+      [folder_path, self.name].join('/')
+    else
+      self.name
+    end
+  end
+
+  private
+
+  def folder_path
+    self.folder.path_ids.map { |id| Folder.find(id).name }.join('/')
   end
 end
--- spec/requests/api/v1/paths_spec.rb
+++ spec/requests/api/v1/paths_spec.rb
@@ -9,26 +9,79 @@ RSpec.describe 'Api::V1::Paths', type: :request do
     before do
       @folder1 = Folder.create(name: 'folder1')
       @folder2 = Folder.create(name: 'folder2', parent: @folder1)
-      @image = Image.create(name: 'image', folder: @folder2)
-      @image2 = Image.create(name: 'image2', folder: @folder2)
-    end
-    
-    it 'returns image json' do
-      get '/api/v1/folder1/folder2/image'
-      expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to have_key('image')
+      @folder3 = Folder.create(name: 'folder3', parent: @folder2)
+
+      @image1 = Image.create(name: 'image1')
+      @image2 = Image.create(name: 'image2', folder: @folder1)
+      @image3 = Image.create(name: 'image3', folder: @folder2)
+      @image4 = Image.create(name: 'image4', folder: @folder2)
     end
 
-    it 'returns folder and image json' do
-      get "/api/v1/folder1/folder2"
-      expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to have_key('folder')
+    context 'root level folder' do
+      before do
+        get '/api/v1/folder1'
+      end
+
+      let(:root_images) { Image.where(folder: @folder1) }
+
+      it 'returns folder json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['folder']['name']).to eq(@folder1.name)
+      end
+
+      it 'returns images json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['images'].count).to equal(root_images.count)
+      end
     end
 
-    it 'returns empty json ' do
-      get "/api/v1/folder1/folder2/nonexistentfolder"
-      expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to be_empty
+    context 'root level image' do
+      before do
+        get '/api/v1/image1'
+      end
+
+      it 'returns image json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['image']['name']).to eq(@image1.name)
+      end
+    end
+
+    context '3rd level folder' do
+      before do
+        get '/api/v1/folder1/folder2/folder3'
+      end
+
+      let(:l3_images) { Image.where(folder: @folder3) }
+
+      it 'returns folder json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['folder']['name']).to eq(@folder3.name)
+      end
+
+      it 'returns images json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['images'].count).to equal(l3_images.count)
+      end
+
+    end
+
+    context '3rd level image' do
+      before do
+        get '/api/v1/folder1/folder2/image3'
+      end
+
+      it 'returns image json' do
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['image']['name']).to eq(@image3.name)
+      end
+    end
+
+    context 'cannot find resource' do
+      it 'returns empty json ' do
+        get "/api/v1/folder1/folder2/nonexistentfolder"
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)).to be_empty
+      end
     end
   end
 end
