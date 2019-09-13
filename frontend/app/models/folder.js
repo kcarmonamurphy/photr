7b3f1d2bc621ed812b4b44ebc8bc637b9dd981e0
Kevin Carmona-Murphy <kcarmonamurphy@outlook.com>
2019-09-12 23:24:40 -0400

    added tailwind and started building basic table to show hierarchy for folders and images

--- frontend/app/models/folder.js
+++ frontend/app/models/folder.js
@@ -3,5 +3,10 @@ const { Model } = DS;
 
 export default Model.extend({
   name: DS.attr(),
-  url: DS.attr()
+  url: DS.attr(),
+
+  images: DS.hasMany('image'),
+
+  children: DS.hasMany('folder', { inverse: 'parent' }),
+  parent: DS.belongsTo('folder', { inverse: 'children' })
 });
