61bf69a7510e1d7e314617b76caa1332e659e50c
Kevin Carmona-Murphy <kcarmonamurphy@outlook.com>
2019-10-14 21:13:37 -0400

    drag and drop seems to be working, but without transition

--- frontend/app/components/ui-icon.js
+++ frontend/app/components/ui-icon.js
@@ -3,11 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: '',
   name: '',
-  // Update iconSize to fixed sizes (small, default, large, xl)
-  // For now default default sizes are:
-  // Small: 4
-  // Default: 6
-  iconSize: '6',
 
+  iconSize: '6',
   iconColor: 'black',
 });
