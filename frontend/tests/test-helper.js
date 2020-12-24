7e3a3fe9c6b4cfa4ff7d34f191aff09100ce0d83
Kevin Carmona-Murphy <kcarmonamurphy@outlook.com>
2020-12-23 23:36:42 -0500

    upgrade to ember 3.20

--- frontend/tests/test-helper.js
+++ frontend/tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'photr/app';
+import config from 'photr/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
